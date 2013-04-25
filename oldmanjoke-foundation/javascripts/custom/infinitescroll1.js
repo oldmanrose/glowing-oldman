4f138855a30907b3a6fe75e2ff62d07e1891a0be
Mark Rose <oldmanrose@Marks-MacBook-Air.local>
2013-04-25 19:52:28 -0400

    final project draft 1

---
+++ oldmanjoke-foundation/javascripts/custom/infinitescroll1.js
@@ -0,0 +1,28 @@
+<script type="text/javascript">
+$(document).ready(function(){
+    function lastAddedLiveFunc()
+    {
+        $('div#lastPostsLoader').html('<img src="bigLoader.gif">');
+
+        $.get("loadmore.php", function(data){
+            if (data != "") {
+                //console.log('add data..');
+                $(".items").append(data);
+            }
+            $('div#lastPostsLoader').empty();
+        });
+    };
+
+    //lastAddedLiveFunc();
+    $(window).scroll(function(){
+
+        var wintop = $(window).scrollTop(), docheight = $(document).height(), winheight = $(window).height();
+        var  scrolltrigger = 0.95;
+
+        if  ((wintop/(docheight-winheight)) > scrolltrigger) {
+         //console.log('scroll bottom');
+         lastAddedLiveFunc();
+        }
+    });
+});
+</script>
